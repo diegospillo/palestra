4a36873d8883fe982707338a240040e22a9085e4
diegospillo <diego12juve@hotmail.com>
2023-02-01 21:26:32 +0100

    First Commit

--- mysql/query.js
+++ mysql/query.js
@@ -4,15 +4,15 @@ module.exports = {
     ins : (id,peso,serie,ripetizioni) =>{
       //console.log("Insert")
       con.query(`UPDATE scheda SET peso = '${peso}', serie = '${serie}', ripetizioni = '${ripetizioni}'  WHERE id = '${id}'`, (err, result, fields) => {
-        //if (err) throw err;
+        if (err) throw err;
         console.log("Success");
       });
     },
     select: () =>{return new Promise((resolve, reject) => {
       //console.log("Select")
-      con.query("SELECT * FROM scheda2", (err, result, fields) => {
-        //if (err) throw err;
-         console.log(result);
+      con.query("SELECT * FROM scheda", (err, result, fields) => {
+        if (err) throw err;
+         //console.log(con);
           resolve(result);
       })
     })
