9764b2ea958354162569b8b805b91ffd91c7f2c4
diegospillo <diego12juve@hotmail.com>
2023-02-06 16:13:00 +0100

    First Commit

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ app.set('view engine', 'ejs')
 var scheda = require('./postgreSQL/query.js');
 const dominio = process.env.URL || "http://localhost:3000";
 
-app.get('/', (req, res) => {
+app.get('/', async(req, res) => {
 
     /*scheda.select().then((data) => {
     //res.render('sito', { uscita: data, n_esercizio: "0",s_url:dominio });
     res.json(data); 
   })*/
-  var data = scheda.select();
+  var data = await scheda.select();
   res.json(data);
 })
 
