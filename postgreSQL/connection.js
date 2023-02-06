94d8595d6dbc00b9949ed635a03722f56577f07b
diegospillo <diego12juve@hotmail.com>
2023-02-06 17:23:04 +0100

    First Commit

--- postgreSQL/connection.js
+++ postgreSQL/connection.js
@@ -4,7 +4,7 @@ const client = new Client({
   host: 'dpg-cfg1111a6gdma8lskmvg-a.frankfurt-postgres.render.com',
   database: 'palestra',
   password: 'htBgzUNs9G9GBCFC2nPexpa9j16uNkNP',
-  port: '5432',
+  port: 5432,
 })
 
 const getScheda = (req, res) => {
