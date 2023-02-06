77f88584034a5d3260b8ccf635c358405fb61e6e
diegospillo <diego12juve@hotmail.com>
2023-02-06 16:49:33 +0100

    First Commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 
 //var scheda = require('./postgreSQL/query.js');
-const client = require("./postgreSQLconnection.js");
+const client = require("./postgreSQL/connection.js");
 const dominio = process.env.URL || "http://localhost:3000";
 
 app.get('/', async(req, res) => {
